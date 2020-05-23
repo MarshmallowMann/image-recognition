e5d651c587629cad3edf292f90ca7db7f6601f70
JR Despi <johnrobertson.despi@gmail.com>
2020-05-24 05:58:14 +0800

    finished setting up the basic styles and components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Navigation from "./components/navigation/navigation";
 import './App.css';
+import Logo from "./components/Logo/logo"
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/rank";
+import Particles from "react-particles-js";
+const particleOptions = {
+    "particles": {
+    "number": {
+        "value": 50
+    },
+    "size": {
+        "value": 3
+    }
+},
+    "interactivity": {
+    "events": {
+        "onhover": {
+            "enable": true,
+                "mode": "repulse"
+        }
+    }
+}}
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+          <Particles className="particles"
+              params={particleOptions}/>
         <Navigation />
-        {/* <Logo />
+        <Logo />
+        <Rank />
         <ImageLinkForm />
-        <FaceRecognition /> */}
+        {/*<FaceRecognition /> */}
       </div>
     );
   }
---
+++ src/components/Rank/rank.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Rank = () => {
+    return (
+        <div>
+            <div className="white f3">
+                {"JR, your current rank is..."}
+            </div>
+            <div className="white f1 fw9">
+                {"#5"}
+            </div>
+        </div>);
+}
+export default Rank
